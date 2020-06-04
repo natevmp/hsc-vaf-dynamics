1092ed0d3ac77c3680cd7deca8c85d766ce4392d
Nathaniel <nathanielmonpere@hotmail.com>
2020-06-04 11:57:48 +0200

    Slightly sped up dfs evolution algorithm
    Preallocated cache array of n_f to reduce allocation during timestep.

--- src/evolver.jl
+++ src/evolver.jl
@@ -1,17 +1,15 @@
 using FiniteDiff
 
 """Evolve vector for timestep using Euler method"""
-function stepEuler(n_f::AbstractVector, moveRate_f::AbstractVector, dt::AbstractFloat)
-    # create swap copy
-    nT_f = copy(n_f)
+function stepEuler!(n_f::AbstractVector, nC_f::AbstractVector, moveRate_f::AbstractVector, dt::Real)
     # evolve boundaries
-    n_f[1] = nT_f[1] + moveRate_f[2]dt*nT_f[2]
-    n_f[end] = nT_f[end] + moveRate_f[end-1]dt*nT_f[end-1]
+    n_f[1] = nC_f[1] + moveRate_f[2]dt*nC_f[2]
+    n_f[end] = nC_f[end] + moveRate_f[end-1]dt*nC_f[end-1]
     # evolve body
     for i in 2:length(n_f)-1
-        n_f[i] = (1-2moveRate_f[i]dt)*nT_f[i] +
-        moveRate_f[i-1]dt*nT_f[i-1] +
-        moveRate_f[i+1]dt*nT_f[i+1]
+        n_f[i] = (1-2moveRate_f[i]dt)*nC_f[i] +
+        moveRate_f[i-1]dt*nC_f[i-1] +
+        moveRate_f[i+1]dt*nC_f[i+1]
     end
     return nothing
 end
@@ -22,17 +20,22 @@ function evolveVAF(dfs::DFreqspace, params::Dict, t::Number, dt::Float64; addClo
     N = params["N"]
     μ = params["μ"]
     ϕ = params["ϕ"]
+    n_f = dfs.n_f
+    f_f = dfs.freqs_f
 
     # Calculate frequency dependent move rate
-    probMove(f) = f.*(1. .- f)
-    moveRate_f = ρ * N * probMove(dfs.freqs_f)
+    probMove(f) = f*(1. - f)
+    moveRate_f = ρ * N * probMove.(f_f)
 
+    # Evolve state space
+    nC_f = copy(n_f)
     for tt in dt:dt:t
+        nC_f .= n_f
         # Evolve existing clones
-        stepEuler(dfs.n_f, moveRate_f, dt)
+        stepEuler!(n_f, nC_f, moveRate_f, dt)
         # Add new clones
         if addClones
-            dfs.n_f[2] += N*μ*(ρ+ϕ/2)*dt
+            n_f[2] += N*μ*(ρ+ϕ/2)*dt
         end
     end
     return nothing
@@ -46,16 +49,18 @@ function evolveVAF(cfs::CFreqspace, params::Dict, t::Real, dt::Float64; addClone
     #event rate
     r = ρ * N
     #preallocate temp arrays
-    arg_f = zeros(length(cfs.n_f))
-    nSwap_f = zeros(length(cfs.n_f))
-    g_f = map(x -> x*(1-x), cfs.freqs_f)
+    n_f = cfs.n_f
+    nC_f = zeros(length(n_f))
+    f_f = cfs.freqs_f
+    arg_f = zeros(length(n_f))
+    g_f = map(x -> x*(1-x), f_f)
     newCloneInd = freqToInd(1/N, cfs.df)
 
     for tt in dt:dt:t
-        nSwap_f .= copy(cfs.n_f)
-        arg_f .= g_f .* nSwap_f
+        nC_f .= n_f
+        arg_f .= g_f .* nC_f
         # diffuse step euler
-        cfs.n_f[2:end-1] = nSwap_f[2:end-1] .+
+        cfs.n_f[2:end-1] = nC_f[2:end-1] .+
                             dt*r*cd2(arg_f[2:end-1], arg_f[3:end], arg_f[1:end-2], cfs.df) / N^2
         # add clones
         if addClones
@@ -98,26 +103,27 @@ function evolveGrowingVAF(cfs::CFreqspace, par::Dict, t::Real, dt::Float64; addC
     N0 = par["N0"]
 
     #preallocate
-    l = length(cfs.n_f)
+    n_f = cfs.n_f
+    l = length(n_f)
+    nC_f = zeros(l)
     arg_f = zeros(l)
-    nSwap_f = zeros(l)
     x_f = cfs.freqs_f
     a_f = zeros(l)
     b_f = zeros(l)
+    n = N0
 
     bFP(x, n) = n*( 2ρ/(n^2) + γ/(n+1)^2 )*x*(1-x)
 
     for tt in dt:dt:t
-        nSwap_f .= copy(cfs.n_f)
+        nC_f .= n_f
         # evolve clones
         n = N0*exp(γ*tt)
         a_f .= 0
         b_f .= map(x -> bFP(x, n), x_f)
-        cfs.n_f[2:end-1] = nSwap_f[2:end-1] .+ dt*fpOp(nSwap_f, a_f, b_f, cfs.df)
+        n_f[2:end-1] = nC_f[2:end-1] .+ dt*fpOp(nC_f, a_f, b_f, cfs.df)
         # add clones
         if addClones
-            newCloneInd = freqToInd(1/n, cfs.df)
-            cfs.n_f[1 + newCloneInd] += 2*(ρ + γ)*n*μ*dt * 1/cfs.df
+            cfs.n_f[1 + freqToInd(1/n, cfs.df)] += 2*(ρ + γ)*n*μ*dt * 1/cfs.df
         end
     end
 
